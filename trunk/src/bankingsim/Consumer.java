38df9f49996d8c501e883ac67d45976834c0ac3a
schmidt.eric <schmidt.eric@92a9d362-fa27-11dd-87c9-111d11f6bf44>
2009-02-16 14:08:41 +0000

    part A, first crack

--- trunk/src/bankingsim/Consumer.java
+++ trunk/src/bankingsim/Consumer.java
@@ -1,81 +1,95 @@
 package bankingsim;
 
-public class Consumer extends BankingSim{	
-	public float d[][];
-	public float d_prev[][];
-	public float c[];
-	public float c_prev[];
-	public boolean state_failc[];
-	protected float lc[];   //lc[k]: the total loans to consumer k;
-	protected float dc[];   //lc[k]: the total deposit from consumer k;
+public class Consumer extends BankingSim{
 	
-	public Consumer(){
-				
-		for(int i = 0; i < numBanks; i++){
-			for(int j = 0; j < numConsumers; j++){
-				d[i][j] = 0;
-				d_prev[i][j] = 0;
-			}
-		}
-		
-		for(int i = 0; i < numConsumers; i++){
-			c[i] = 0;
-			c_prev[i] = 0;
-			state_failc[i] = false;
-			lc[i] = 0;
-			dc[i] = 0;
-		}
+	public Consumer() {
+	
 	}
 	
-	public void run(){
-		float C = 0;	//total currency in consumers' hand
+	public void run() {
 		
-		//backup old d[][] and c[]
-		
-		
-		//check if there are any consumers bankrupted
-		countLoans();
-		countDeposits();
-		for(int i = 0; i < numConsumers; i++){
-			c[i] = lc[i] - dc[i];
-			if(c[i] < 0)
-				if(bankrupt(i))	continue;
-			C += c[i];	
+		//Calculate desired currency for each consumer
+		double desiredCurrency[] = new double[numConsumers];
+		double totalCur = 0;
+		for (int c= 0;c<numConsumers;c++ ) {
+			desiredCurrency[c]=initBase/numConsumers*Math.pow(Math.E,rng.nextGaussian());
+			totalCur+=desiredCurrency[c];
+		}
+		for(int c=0;c<numConsumers;c++) {
+			desiredCurrency[c] /= totalCur/initBase; //multiply by cr+1? or by 1/cr+1 or divide by either of these?
 		}
 		
-		
-		//generate new deposits
-		float D = fed.cr * C;
-		for(int i = 0; i < numBanks; i++){
-			for(int j = 0; j < numConsumers; j++){
-				//d[i][j]
+		//given desired currency, consumer either wants to withdraw/borrow or save/pay back loans
+		for(int c =numBanks;c<numBanks+numConsumers;c++) {
+			if(desiredCurrency[c]<currency[numBanks+c]) { // consumer desires less currency
+				if (consumerLoans[c]>0) { // if consumer has some loans taken out
+					for (int b=0;b<numBanks;b++) { // then first try to decrease currency by paying back loans
+						if (loans[b][numBanks+c]>=currency[numBanks+c]-desiredCurrency[c]) {
+							payBackLoan(c,b,currency[numBanks+c]-desiredCurrency[c]);
+							break;
+						} else {
+							payBackLoan(c,b,loans[b][numBanks+c]);
+						}
+					}
+				}
+				if(desiredCurrency[c]<currency[numBanks+c]) //if still desire less currency
+					depositInBank(c,rng.nextInt(numBanks),currency[numBanks+c]-desiredCurrency[c]); // then deposit the difference at some random bank
 			}
 		}
-		
-	}
-	
-	protected void countLoans(){
-		for(int i = 0; i < numConsumers; i++){
-			lc[i] = 0;
-			for(int j = 0; j < numBanks; j++){
-				lc[i] += bank.l[j][i];
-			}
-		}
-	}
-	
-	protected void countDeposits(){
-		for(int i = 0; i < numConsumers; i++){
-			dc[i] = 0;
-			for(int j = 0; j < numBanks; j++){
-				dc[i] += d[j][i];
+		for(int c =numBanks;c<numBanks+numConsumers;c++) {
+			if(desiredCurrency[c]>currency[numBanks+c]) { // consumer desires more currency
+				if(consumerDeposits[c] > 0) { //if consumer has some deposits in some bank
+					for(int b=0;b<numBanks;b++) { //then first try to increase currency by withdrawing deposits
+						if (deposits[c][b]>=desiredCurrency[c]-currency[numBanks+c]) {
+							withdrawFromBank(c,b,desiredCurrency[c]-currency[numBanks+c]);
+							break;
+						} else 
+							withdrawFromBank(c,b,deposits[c][b]);
+					}
+				}
+				if(desiredCurrency[c]>currency[numBanks+c]) { //if still want more currency
+					for(int b=0;b<numBanks;b++) {  //then try to get the rest via loans
+						if(bankDeposits[b]*(1-rr)>bankLoansOut[b]) {
+							if (bankDeposits[b]*(1-rr)-bankLoansOut[b] >= desiredCurrency[c]-currency[numBanks+c]) { 
+								takeOutLoan(c,b,desiredCurrency[c]-currency[numBanks+c]);
+								break;
+							}
+							else
+								takeOutLoan(c,b,bankDeposits[b]*(1-rr)-bankLoansOut[b]);
+						}
+					}
+				}
+				if(desiredCurrency[c]>currency[numBanks+c]) //if STILL want more currency
+					everFailed[numBanks+c]=true; // consumer has failed in a sense
 			}
 		}
 	}
 	
-	//try to call back deposits before bankrupt consumer i
-	//if fail to call back deposits, actually bankrupt consumer, return true
-	//if succeed, don't bankrupt, return false
-	protected boolean bankrupt(int i){
-		return false;
+	public void depositInBank(int c, int b, double amt) {
+		deposits[c][b]+=amt;
+		currency[numBanks+c]-=amt;
+		consumerDeposits[c]+=amt;
+		bankDeposits[b]+=amt;
+	}
+	
+	public void withdrawFromBank(int c, int b, double amt) {
+		deposits[c][b]-=amt;
+		currency[numBanks+c]+=amt;
+		consumerDeposits[c]-=amt;
+		bankDeposits[b]-=amt;
+	}
+	
+	public void payBackLoan(int c, int b, double amt) {
+		loans[b][numBanks+c]-=amt;
+		currency[numBanks+c]-=amt;
+		bankLoansOut[b]-=amt;
+		consumerLoans[c]-=amt;
+	}
+	
+	public void takeOutLoan(int c, int b, double amt) {
+		loans[b][numBanks+c]+=amt;
+		currency[numBanks+c]+=amt;
+		bankLoansOut[b]+=amt;
+		consumerLoans[c]+=amt;
 	}
 }
