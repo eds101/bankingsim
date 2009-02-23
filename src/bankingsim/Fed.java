f4a73c27cc1fadec0b93f0ea36a949da1bb1a24e
schmidt.eric <schmidt.eric@92a9d362-fa27-11dd-87c9-111d11f6bf44>
2009-02-23 12:08:47 +0000

    part A done except for (1) need to format output to 2 decimal places (2) need graphs and summary

--- src/bankingsim/Fed.java
+++ src/bankingsim/Fed.java
@@ -1,14 +1,21 @@
 package bankingsim;
 
 
-public class Fed extends BankingSim {
+public class Fed {
 		
-	public Fed(){
-		
+	private BankingSim sim;
+	
+	public Fed(BankingSim sim){
+		this.sim = sim;
 	}
 	
 	public void run(){
-		rr = 0.01*rng.nextInt(11);
+		sim.rr += (sim.rng.nextDouble()-.5)*.01; //changes between -.5% and +.5%
+		if(sim.rr<0) {
+			sim.rr = 0;
+		} else if (sim.rr > 1) {
+			sim.rr = 1;
+		}
 	}
 
 }
